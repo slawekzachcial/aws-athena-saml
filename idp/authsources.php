70bd6852a9ef0c655e6169448d3a249267edf4f4
Slawek Zachcial <slawek.zachcial@gmail.com>
2023-01-18 13:44:12 +0100

    Add user3 as Redshift user

--- idp/authsources.php
+++ idp/authsources.php
@@ -20,6 +20,19 @@ $config = array(
             'eduPersonAffiliation' => array('group2'),
             'email' => 'user2@example.com',
         ),
+        'user3:user3pass' => array(
+            'uid' => array('3'),
+            'eduPersonAffiliation' => array('group2'),
+            'email' => 'user3@example.com',
+            'https://aws.amazon.com/SAML/Attributes/Role' => array(
+                'arn:aws:iam::'.getenv('AWS_ACCOUNT_ID').':role/redshift-saml-noaccess-role,arn:aws:iam::'.getenv('AWS_ACCOUNT_ID').':saml-provider/redshift-saml-idp',
+                'arn:aws:iam::'.getenv('AWS_ACCOUNT_ID').':role/redshift-saml-role,arn:aws:iam::'.getenv('AWS_ACCOUNT_ID').':saml-provider/redshift-saml-idp'
+            ),
+            'https://aws.amazon.com/SAML/Attributes/RoleSessionName' => 'user3@example.com',
+            // 'https://redshift.amazon.com/SAML/Attributes/DbUser' => 'user3@example.com',
+            // 'https://redshift.amazon.com/SAML/Attributes/AutoCreate' => 'true',
+            // 'https://redshift.amazon.com/SAML/Attributes/DbGroups' => array('readonly_group'),
+        ),
     ),
 
 );
